fea7acec6219110b0ca6316f82717dca877832b7
mumu <13630064412@163.com>
2018-06-23 17:58:26 +0800

    第一次提交.

--- app/src/main/java/com/example/ridko/warehousepda/fragment/UserFragment.java
+++ app/src/main/java/com/example/ridko/warehousepda/fragment/UserFragment.java
@@ -10,21 +10,41 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.ridko.warehousepda.R;
-import com.example.ridko.warehousepda.adapter.BasePullUpRecyclerAdapter;
 import com.example.ridko.warehousepda.entity.Managment;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by mumu on 2018/6/22.
  */
 
 public class UserFragment extends Fragment {
+    @Bind(R.id.iv)
+    ImageView iv;
+    @Bind(R.id.tv1)
+    TextView tv1;
+    @Bind(R.id.tv2)
+    TextView tv2;
+    @Bind(R.id.tv3)
+    TextView tv3;
+    @Bind(R.id.tv4)
+    TextView tv4;
+    @Bind(R.id.linearlayout)
+    LinearLayout linearlayout;
+    @Bind(R.id.button)
+    Button button;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,8 +63,10 @@ public class UserFragment extends Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
     }
-    private List<Managment> list=new ArrayList<>();
+
+    private List<Managment> list = new ArrayList<>();
     private DeviceManagmentFragment.ManagementAdapter mgAdapter;
+
     //这里写界面
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -66,5 +88,15 @@ public class UserFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.linearlayout, R.id.button})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.linearlayout:
+                break;
+            case R.id.button:
+                break;
+        }
     }
 }
