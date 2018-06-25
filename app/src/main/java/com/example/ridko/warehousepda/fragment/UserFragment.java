3d12049d38b15ce53e01a415d1c21d70d6869d7b
mumu <13630064412@163.com>
2018-06-25 18:30:40 +0800

    第一次提交.

--- app/src/main/java/com/example/ridko/warehousepda/fragment/UserFragment.java
+++ app/src/main/java/com/example/ridko/warehousepda/fragment/UserFragment.java
@@ -1,9 +1,11 @@
 package com.example.ridko.warehousepda.fragment;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,11 +13,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ridko.warehousepda.R;
+import com.example.ridko.warehousepda.application.App;
 import com.example.ridko.warehousepda.entity.Managment;
 
 import java.util.ArrayList;
@@ -64,9 +69,6 @@ public class UserFragment extends Fragment {
         super.onAttach(activity);
     }
 
-    private List<Managment> list = new ArrayList<>();
-    private DeviceManagmentFragment.ManagementAdapter mgAdapter;
-
     //这里写界面
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -94,9 +96,47 @@ public class UserFragment extends Fragment {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.linearlayout:
+                blinkDialog();
                 break;
             case R.id.button:
                 break;
         }
+    }
+    private void blinkDialog(){
+        final Dialog dialog;
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View blinkView = inflater.inflate(R.layout.dialog_change_prassword, null);
+        Button no=(Button) blinkView.findViewById(R.id.dialog_no);
+        Button yes=(Button) blinkView.findViewById(R.id.dialog_yes);
+        final EditText edit1=(EditText)blinkView.findViewById(R.id.edit1);
+        final EditText edit2=(EditText)blinkView.findViewById(R.id.edit2);
+        final EditText edit3=(EditText)blinkView.findViewById(R.id.edit3);
+        dialog = new AlertDialog.Builder(getActivity()).create();
+        dialog.show();
+        dialog.getWindow().setContentView(blinkView);
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String oldPrassword=edit1.getText().toString();
+                String newPrassword=edit2.getText().toString();
+                String samePrassword=edit3.getText().toString();
+                if (newPrassword!=null&&newPrassword.equals(samePrassword)){
+                if (oldPrassword!=null){
+//                    判断旧密码是否正确
+                }else {
+                    App.toastShow(getContext(),getResources().getString(R.string.text29), Toast.LENGTH_SHORT);
+                }
+                dialog.dismiss();
+                }else {
+                    App.toastShow(getContext(),getResources().getString(R.string.text28), Toast.LENGTH_SHORT);
+                }
+            }
+        });
     }
 }
