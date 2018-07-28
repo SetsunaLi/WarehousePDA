9362247af25e6044591263963e978281b3dfc9d4
mumu <13630064412@163.com>
2018-07-28 15:38:21 +0800

    2018/7/28

--- app/src/main/java/com/example/ridko/warehousepda/fragment/RuKuFragement.java
+++ app/src/main/java/com/example/ridko/warehousepda/fragment/RuKuFragement.java
@@ -1,16 +1,23 @@
 package com.example.ridko.warehousepda.fragment;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.ridko.warehousepda.R;
 import com.example.ridko.warehousepda.adapter.ListDataAdapter;
@@ -18,20 +25,45 @@ import com.example.ridko.warehousepda.entity.ListEntity;
 
 import java.util.ArrayList;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by mumu on 2018/7/24.
  */
 
 public class RuKuFragement extends Fragment {
+    @Bind(R.id.text1)
+    TextView text1;
+    @Bind(R.id.edit1)
+    EditText edit1;
+    @Bind(R.id.spinner1)
+    Spinner spinner1;
+    @Bind(R.id.spinner2)
+    Spinner spinner2;
+    @Bind(R.id.spinner3)
+    Spinner spinner3;
+    @Bind(R.id.edit2)
+    EditText edit2;
+    @Bind(R.id.edit3)
+    EditText edit3;
+    @Bind(R.id.edit4)
+    EditText edit4;
+    @Bind(R.id.edit5)
+    EditText edit5;
+    @Bind(R.id.button_ok)
+    Button buttonOk;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-    private ArrayList<ListEntity> mlist=new ArrayList<>();
+
+    private ArrayList<ListEntity> mlist = new ArrayList<>();
     private ListDataAdapter mAdapter;
-    private int selectID=0;
+    private int selectID = 0;
+
     //    这里加载视图
     @Nullable
     @Override
@@ -70,4 +102,46 @@ public class RuKuFragement extends Fragment {
         ButterKnife.unbind(this);
     }
 
+    @OnClick({R.id.button_ok,})
+    public void onViewClicked(View view) {
+        switch (view.getId()){
+            case R.id.button_ok:
+                blinkDialog();
+                break;
+        }
+    }
+    private void blinkDialog() {
+        final Dialog dialog;
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View blinkView = inflater.inflate(R.layout.dialog_bind, null);
+        Button no = (Button) blinkView.findViewById(R.id.dialog_no);
+        Button yes = (Button) blinkView.findViewById(R.id.dialog_yes);
+        final EditText editNo = (EditText) blinkView.findViewById(R.id.editNO);
+        ImageView img = (ImageView) blinkView.findViewById(R.id.imgSearch);
+
+        dialog = new AlertDialog.Builder(getActivity()).create();
+        dialog.show();
+        dialog.getWindow().setContentView(blinkView);
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                KeyEvent newEvent = new KeyEvent(KeyEvent.ACTION_DOWN,
+                        KeyEvent.KEYCODE_BACK);
+                getActivity().onKeyDown(KeyEvent.KEYCODE_BACK, newEvent);
+            }
+        });
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                询标开始
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/ridko/warehousepda/fragment/SpecialFragment3.java
+++ app/src/main/java/com/example/ridko/warehousepda/fragment/SpecialFragment3.java
@@ -1,10 +1,12 @@
 package com.example.ridko.warehousepda.fragment;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.ridko.warehousepda.R;
 import com.example.ridko.warehousepda.adapter.ListDataAdapter;
@@ -110,7 +113,33 @@ public class SpecialFragment3 extends Fragment {
                 transaction.commit();
                 break;
             case R.id.button_ok:
+                blinkDialog();
                 break;
         }
+    }
+    private void blinkDialog(){
+        final Dialog dialog;
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View blinkView = inflater.inflate(R.layout.dialog_stock_removal, null);
+        Button no=(Button) blinkView.findViewById(R.id.dialog_no);
+        Button yes=(Button) blinkView.findViewById(R.id.dialog_yes);
+        dialog = new AlertDialog.Builder(getActivity()).create();
+        TextView text=(TextView)blinkView.findViewById(R.id.dialog_text);
+        text.setText(R.string.text6);
+        dialog.show();
+        dialog.getWindow().setContentView(blinkView);
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+//                发送完成订单单号，生成出库单
+            }
+        });
     }
 }
